2c92fbde5e101b91adb27ca1aec0377ce653cc31
Roberto Gazia <r.gazia@arduino.cc>
2026-05-28 01:34:30 +0200

    3.1_add-sketch

---
+++ examples/rss-reader/sketch/sketch.ino
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: Copyright (C) Arduino s.r.l. and/or its affiliated companies
+//
+// SPDX-License-Identifier: MPL-2.0
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("RSS reader sketch ready");
+}
+
+void loop() {
+  delay(1000);
+}
