2d61f5ecaf40f94547216dee1e58249486f98715
Roberto Gazia <r.gazia@arduino.cc>
2026-05-28 01:44:42 +0200

    3.4_matrix-icon

---
+++ examples/rss-reader/sketch/frames.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (C) Arduino s.r.l. and/or its affiliated companies
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+// Placeholder icon shown when there are unread articles.
+// Reuses an existing LED-matrix frame; swap in your own glyph later.
+const uint32_t unread_icon[] = {
+    0x0f808209,
+    0x484042fa,
+    0x14509c82,
+    0x08000000,
+};
--- examples/rss-reader/sketch/sketch.ino
+++ examples/rss-reader/sketch/sketch.ino
@@ -3,6 +3,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
 #include <Arduino_RouterBridge.h>
+#include <Arduino_LED_Matrix.h>
+#include "frames.h"
+
+Arduino_LED_Matrix matrix;
 
 volatile int unread_count = 0;
 
@@ -14,19 +18,19 @@ void on_unread_count(int count) {
 
 void setup() {
   Serial.begin(9600);
+
+  matrix.begin();
+  matrix.clear();
+
   Bridge.begin();
   Bridge.provide("unread_count", on_unread_count);
 }
 
 void loop() {
   if (unread_count > 0) {
-    // Blink while there are unread articles.
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(300);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(300);
+    matrix.loadFrame(unread_icon);
   } else {
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(300);
+    matrix.clear();
   }
+  delay(300);
 }
